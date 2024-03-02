06a5c7a482ea699ce6602e14b63aedceb8dc105b
luminuszz <luminuszz43@gmail.com>
2024-03-01 22:21:25 -0300

    feat: add telegram integration button

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -127,9 +127,7 @@ const SelectItem = React.forwardRef<
       </SelectPrimitive.ItemIndicator>
     </span>
 
-    <SelectPrimitive.ItemText>
-      <span>{children}</span>
-    </SelectPrimitive.ItemText>
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
 ))
 SelectItem.displayName = SelectPrimitive.Item.displayName
