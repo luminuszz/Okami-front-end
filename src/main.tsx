48f6aead650e760c62b5a13cd15910d5a57de14a
luminuszz <luminuszz43@gmail.com>
2024-03-18 23:05:41 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -7,5 +8,6 @@ import { App } from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
