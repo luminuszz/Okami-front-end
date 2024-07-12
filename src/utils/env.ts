1cdf3cc1a8ab02745c28624927e9c8a53b0ba3af
luminuszz <luminuszz43@gmail.com>
2024-07-12 01:07:22 -0300

    fix:  type env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   VITE_API_URL: z.string(),
   VITE_STRIPE_PUBLIC_KEY: z.string(),
   VITE_DEFAULT_WORK_IMAGE: z.string(),
-  VITE_IS_DEV: z.coerce.boolean().optional().default(false),
+  VITE_IS_DEV: z.string().or(z.coerce.boolean()).optional(),
 })
 
 export type Env = z.infer<typeof envSchema>
