5b6fde3e5eed2468af774c46380c4dde09a5d15b
luminuszz <luminuszz43@gmail.com>
2024-11-08 14:27:28 -0300

    fix(posthog): remove post hog

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,8 +3,6 @@ import { z } from 'zod'
 export const envSchema = z.object({
   VITE_API_URL: z.string(),
   VITE_IS_DEV: z.string().or(z.coerce.boolean()).optional(),
-  VITE_PUBLIC_POSTHOG_HOST: z.string(),
-  VITE_PUBLIC_POSTHOG_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
